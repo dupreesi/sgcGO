7f5af28d22e163ea38f66f194f726a66d4f4ab85
Michael Watts <hello@michaelwatts.co>
2018-08-24 13:03:56 +0100

    add responsive breakpoints, animate app drawer, add custom radio button styling

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,26 +1,36 @@
 /* eslint-disable */
 
-var join = document.getElementById("join");
-var joinBoardInput = document.getElementById("board_name");
+/* Hide input drawer and show + icon */
 
-var joinTable = function() {
-  if (joinBoardInput.value) {
-    var boardName = joinBoardInput.value;
-    window.location.href = "/board/" + boardName;
-  }
-};
-
-joinBoardInput.addEventListener("click", function(event) {
-  event.stopPropagation();
+var footer = document.getElementById('add_topic');
+footer.classList.add('footer--hidden');
+var drawerButton = document.getElementById('drawerButton');
+drawerButton.addEventListener('click', function() {
+  footer.classList.toggle('footer--hidden');
+  drawerButton.classList.toggle('drawer-button--hidden');
 });
 
-joinBoardInput.addEventListener("keypress", function(event) {
-  var key = event.which || event.keyCode;
-  if (key === 13) {
-    joinTable();
-  }
-});
+// var join = document.getElementById('join');
+// var joinBoardInput = document.getElementById('board_name');
 
-join.addEventListener("click", function(event) {
-  joinTable();
-});
+// var joinTable = function() {
+//   if (joinBoardInput.value) {
+//     var boardName = joinBoardInput.value;
+//     window.location.href = '/board/' + boardName;
+//   }
+// };
+
+// joinBoardInput.addEventListener('click', function(event) {
+//   event.stopPropagation();
+// });
+
+// joinBoardInput.addEventListener('keypress', function(event) {
+//   var key = event.which || event.keyCode;
+//   if (key === 13) {
+//     joinTable();
+//   }
+// });
+
+// join.addEventListener('click', function(event) {
+//   joinTable();
+// });
